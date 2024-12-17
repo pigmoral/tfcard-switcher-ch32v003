5e8de4e2764407886de9e12d5d05ab7a45da23bc
judehahh <liujh2818@outlook.com>
2024-12-17 22:00:56 +0800

    Initial commit, add hw and sw

---
+++ sw/src/main.rs
@@ -0,0 +1,35 @@
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+#![feature(impl_trait_in_assoc_type)]
+
+use ch32_hal as hal;
+use embassy_executor::Spawner;
+use hal::exti::ExtiInput;
+use hal::gpio::{Level, Output, Pull};
+
+#[embassy_executor::main(entry = "qingke_rt::entry")]
+async fn main(spawner: Spawner) -> ! {
+    let mut config = hal::Config::default();
+    config.rcc = hal::rcc::Config::SYSCLK_FREQ_48MHZ_HSI;
+    let p = hal::init(config);
+
+    let mut read_vcc_en = Output::new(p.PC4, Level::Low, Default::default());
+    let mut sel_led = Output::new(p.PC5, Level::High, Default::default());
+    let mut mux_sel = Output::new(p.PC6, Level::Low, Default::default());
+    let mut sel_btn = ExtiInput::new(p.PC7, p.EXTI7, Pull::None);
+
+    loop {
+        sel_btn.wait_for_falling_edge().await;
+        read_vcc_en.toggle();
+        sel_led.toggle();
+        mux_sel.toggle();
+    }
+}
+
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    let _ = hal::println!("\n\n\n{}", info);
+
+    loop {}
+}
